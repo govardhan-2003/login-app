f14118bb5397fe93947f260fbda55738d71adcca
Govardhan <75973815+govardhan-2003@users.noreply.github.com>
2022-07-24 03:06:53 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,14 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		
+		vite: {
+		    define: {
+			'process.env': process.env,
+		    },
+		},
 		adapter: adapter()
+
 	}
 };
 
